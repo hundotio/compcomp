a1a7bffdd1799d23f4db5a647d77972626e64074
hun <96307763+hundotio@users.noreply.github.com>
2025-01-16 14:16:07 -0600

    Add files via upload

---
+++ tools/install.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/lgandx/Responder.git
+git clone https://github.com/topotam/PetitPotam.git
+git clone https://github.com/abaker2010/impacket-fixed.git
+git clone https://github.com/insidetrust/statistically-likely-usernames.git
+git clone https://github.com/the-useless-one/pywerview.git
+git clone https://github.com/gentilkiwi/mimikatz.git
+git clone https://github.com/GhostPack/Rubeus.git
+git clone https://github.com/safebuffer/sam-the-admin.git
+git clone https://github.com/abaker2010/krbrelayx.git
+git clone https://github.com/Ridter/noPac.git
